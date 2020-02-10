96e511469173cca70d824203df1472f60e556a45
ganintsev <ganintsev@yandex-team.ru>
2020-02-10 16:53:13 +0500

    hw04: LogDemo

---
+++ hw04-log/src/main/java/ru/otus/hw04/CalculatorImplWithoutLog.java
@@ -0,0 +1,18 @@
+package ru.otus.hw04;
+
+public class CalculatorWithoutLog implements Calculator {
+    @Override
+    public int calculation(int param) {
+        return param;
+    }
+
+    @Override
+    public int calculation(String param1, int param2, Object param3) {
+        return param2;
+    }
+
+    @Override
+    public int methodWithoutLog() {
+        return 0;
+    }
+}
---
+++ hw04-log/src/main/java/ru/otus/hw04/MyLog.java
@@ -0,0 +1,9 @@
+package ru.otus.hw04;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+public @interface MyLog {
+}
