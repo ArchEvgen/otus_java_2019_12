0835e3e305e5a077af82d12eeb676b0ef0d2e123
ganintsev <ganintsev@yandex-team.ru>
2020-02-12 01:57:47 +0500

    target

--- hw04-log/src/main/java/ru/otus/hw04/MyLog.java
+++ hw04-log/src/main/java/ru/otus/hw04/MyLog.java
@@ -1,9 +1,11 @@
 package ru.otus.hw04;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-@Retention(RUNTIME)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface MyLog {
 }
