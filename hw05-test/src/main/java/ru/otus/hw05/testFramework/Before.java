cabe340565335e581f9ab915a1b4f9d3cffca12d
ganintsev <ganintsev@yandex-team.ru>
2020-02-12 01:39:19 +0500

    + target

--- hw05-test/src/main/java/ru/otus/hw05/testFramework/Before.java
+++ hw05-test/src/main/java/ru/otus/hw05/testFramework/Before.java
@@ -1,9 +1,12 @@
 package ru.otus.hw05.testFramework;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Before {
 }
