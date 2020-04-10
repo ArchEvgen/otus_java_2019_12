4725e17798b1dc3457a4548059532c3e0b51669d
ganintsev <ganintsev@yandex-team.ru>
2020-04-11 00:38:00 +0500

    fix

--- hw09-orm/src/main/java/ru/otus/hw09/orm/JdbcMapper.java
+++ hw09-orm/src/main/java/ru/otus/hw09/orm/JdbcMapper.java
@@ -2,7 +2,6 @@ package ru.otus.hw09.orm;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
